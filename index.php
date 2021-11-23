9cb5d339e112195bcb3ddf906d3614ee9be2f968
gaelv <gaelvn2020@outlook.com>
2021-11-22 21:45:06 -0600

    Mi primer Login con CSS

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Registro</title>
+</head>
+
+<body>
+    <form method="post" class="formulario">
+        <h1 class="titulo__formulario">Bienvenido</h1>
+        <input type="text" name="name" placeholder="Nombre completo">
+        <input type="email" name="email" placeholder="Email">
+        <input type="submit" name="register">
+    </form>
+</body>
+
+</html>
